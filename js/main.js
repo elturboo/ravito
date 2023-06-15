1a220c43fa50a53c06132f545db67f41577fcafc
Mahmoud Awad <m.awad@tamkeentech.sa>
2023-06-15 15:30:59 +0300

    update room menu

--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
 
-// add child
-$(document).on('click', '.addChild', function () {
-    $(this).parent().parent().append(`<div class="d-flex gap-4 align-items-center">
-    <select class="add_child " name="" id="">
-        <option hidden>Add a child</option>
-        <option value="0">0 years</option>
-        <option value="1">1 years</option>
-        <option value="2">2 years</option>
-        <option value="3">3 years</option>
-    </select>
-    <button type="button" class="removeChild btn p-0 red_color"><i class="fa-solid fa-times"></i></button>
-    </div>`);
-});
 
-// remove child
-$(document).on('click', '.removeChild', function () {
-    $(this).parent().remove();
-});
 
+
+
+// get child age
+function val() {
+    childAge = document.getElementById("add_child_select").value;
+    return childAge;
+}
+let childrenNums = 0
 // add room
 let roomNum = 1;
 $(document).on('click', '.addRoom', function () {
@@ -44,15 +35,15 @@ $(document).on('click', '.addRoom', function () {
             <div class="col-6">
                 <label for="">Children</label>
                 <div class="d-flex gap-4 align-items-center">
-                        <select class="add_child " name="" id="">
+                        <select onchange="val()"  class="add_child " name="" id="add_child_select">
                             <option hidden>Add a child</option>
                             <option value="0">0 years</option>
-                            <option value="1" selected>1 years</option>
+                            <option value="1">1 years</option>
                             <option value="2">2 years</option>
                             <option value="3">3 years</option>
                         </select>
-                        <button type="button" class="addChild btn p-0"><i class="fa-solid fa-plus"></i></button>
-                </div>
+                        <button id="add_child_btn" type="button" class="addChild btn p-0"><i
+                        class="fa-solid fa-plus"></i></button>                </div>
             </div>
         </div>
     </div>`);
@@ -95,3 +86,41 @@ decrease.forEach(function (plus, index) {
         decreaseValue(index);
     });
 })
+
+
+
+// remove child
+$(document).on('click', '.removeChild', function () {
+    $(this).parent().remove();
+    childrenNums--
+    console.log(childrenNums);
+    if (childrenNums >= 4) {
+        $('#add_child_btn').css({'display': 'none'});
+        $('#add_child_select').css({'display': 'none'});
+    } else {
+        $('#add_child_btn').css({'display': 'block'});
+        $('#add_child_select').css({'display': 'block'});
+    }
+});
+// add child
+$(document).on('click', '.addChild', function () {
+    if(childAge>=0) {
+        $(this).parent().parent().prepend(`<div class="d-flex gap-4 align-items-center">
+        <span class="child_badge">
+        ${childAge} ${childAge > 1? "years" : "year"}
+        <button type="button" class="removeChild btn p-0 red_color"><i class="fa-solid fa-times"></i></button>
+        </span>
+        </div>`);
+    } 
+    childrenNums++
+
+    console.log(childrenNums);
+    if (childrenNums >= 4) {
+        $('#add_child_btn').css({'display': 'none'});
+        $('#add_child_select').css({'display': 'none'});
+    } else {
+        $('#add_child_btn').css({'display': 'block'});
+        $('#add_child_select').css({'display': 'block'});
+    }
+});
+
